f80e994dd580f807ed9fc816759d243b28d4e5c6
Nils Van Geele <nils@nvgeele.be>
2016-04-23 00:50:26 +0200

    Simple script for spawning threads simulating random citizens

---
+++ src/examples/spawner.clj
@@ -0,0 +1,21 @@
+(ns examples.spawner
+  (:require [tamura.core :as t])
+  (:import [redis.clients.jedis JedisPool Jedis]))
+
+(defn spawn-thread
+  [host queue id]
+  (let [conn (Jedis. host)]
+    (t/threadloop []
+      (.lpush conn queue (into-array String [(str {:user-id id :position [(Math/random) (Math/random)]})]))
+      (Thread/sleep 1000)
+      (recur))))
+
+(defn spawn-threads
+  [host queue num]
+  (doall (for [i (range 0 num)]
+           (spawn-thread host queue i))))
+
+(defn -main
+  [& args]
+  (spawn-threads "localhost" "bxlqueue" 10)
+  (println "Done!"))
