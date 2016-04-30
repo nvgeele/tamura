98962f0789577d978a3e0fd19a06db76791fa7a9
Nils Van Geele <nils@nvgeele.be>
2016-04-30 19:24:52 +0200

    cleaner tests

--- test/tamura/core_test.clj
+++ test/tamura/core_test.clj
@@ -4,50 +4,51 @@
             [clojure.core.async :as a :refer [>!! <!!]]
             [multiset.core :as ms]))
 
+(def ^:dynamic *source-id* nil)
+(def ^:dynamic *source-chan* nil)
+(def ^:dynamic *test-chan* nil)
+
+(defmacro test-node
+  [node-init & body]
+  `(let [source-node# (core/make-source-node)]
+     (binding [*source-id* (:id source-node#)
+               *source-chan* (:in source-node#)
+               *test-chan* (core/chan)]
+       (core/node-subscribe (~node-init source-node#) *test-chan*)
+
+       ~@body)))
+
 (facts
   "about make-delay-node"
   (facts
     "about make-delay-node with multisets"
-    (let [source-node (core/make-source-node)
-          delay-node (core/make-delay-node source-node)
-          test-chan (core/chan)
-          source-chan (:in source-node)
-          id (:id source-node)]
+    (test-node #(core/make-delay-node %)
+      (>!! *source-chan* {:destination *source-id* :value (core/make-multiset (ms/multiset))})
+      (:multiset (:value (<!! *test-chan*))) => (ms/multiset)
 
-      (core/node-subscribe delay-node test-chan)
+      (>!! *source-chan* {:destination *source-id* :value (core/make-multiset (ms/multiset 1))})
+      (:multiset (:value (<!! *test-chan*))) => (ms/multiset)
 
-      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset))})
-      (:multiset (:value (<!! test-chan))) => (ms/multiset)
+      (>!! *source-chan* {:destination *source-id* :value (core/make-multiset (ms/multiset 1 2))})
+      (:multiset (:value (<!! *test-chan*))) => (ms/multiset 1)
 
-      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset 1))})
-      (:multiset (:value (<!! test-chan))) => (ms/multiset)
-
-      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset 1 2))})
-      (:multiset (:value (<!! test-chan))) => (ms/multiset 1)
-
-      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset 1 2 3))})
-      (:multiset (:value (<!! test-chan))) => (ms/multiset 1 2)))
+      (>!! *source-chan* {:destination *source-id* :value (core/make-multiset (ms/multiset 1 2 3))})
+      (:multiset (:value (<!! *test-chan*))) => (ms/multiset 1 2)))
+  
   (facts
     "about make-delay-node with hashes"
-    (let [source-node (core/make-source-node)
-          delay-node (core/make-delay-node source-node)
-          test-chan (core/chan)
-          source-chan (:in source-node)
-          id (:id source-node)]
+    (test-node #(core/make-delay-node %)
+      (>!! *source-chan* {:destination *source-id* :value (core/make-hash {})})
+      (:hash (:value (<!! *test-chan*))) => {}
 
-      (core/node-subscribe delay-node test-chan)
+      (>!! *source-chan* {:destination *source-id* :value (core/make-hash {1 {:v 1}})})
+      (:hash (:value (<!! *test-chan*))) => {}
 
-      (>!! source-chan {:destination id :value (core/make-hash {})})
-      (:hash (:value (<!! test-chan))) => {}
+      (>!! *source-chan* {:destination *source-id* :value (core/make-hash {1 {:v 1} 2 {:v 1}})})
+      (:hash (:value (<!! *test-chan*))) => {}
 
-      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 1}})})
-      (:hash (:value (<!! test-chan))) => {}
+      (>!! *source-chan* {:destination *source-id* :value (core/make-hash {1 {:v 2} 2 {:v 1}})})
+      (:hash (:value (<!! *test-chan*))) => {1 {:v 1}}
 
-      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 1} 2 {:v 1}})})
-      (:hash (:value (<!! test-chan))) => {}
-
-      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 2} 2 {:v 1}})})
-      (:hash (:value (<!! test-chan))) => {1 {:v 1}}
-
-      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 2} 2 {:v 2}})})
-      (:hash (:value (<!! test-chan))) => {1 {:v 1} 2 {:v 1}})))
+      (>!! *source-chan* {:destination *source-id* :value (core/make-hash {1 {:v 2} 2 {:v 2}})})
+      (:hash (:value (<!! *test-chan*))) => {1 {:v 1} 2 {:v 1}})))
