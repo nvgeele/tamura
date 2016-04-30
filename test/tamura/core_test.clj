8373de09d35c17d047e9f0300bff8585f21b9d8d
Nils Van Geele <nils@nvgeele.be>
2016-04-30 18:42:30 +0200

    Tests for the delay node

--- test/tamura/core_test.clj
+++ test/tamura/core_test.clj
@@ -1,10 +1,8 @@
 (ns tamura.core-test
   (:use midje.sweet)
   (:require [tamura.core :as core]
-            [clojure.core.async :as a :refer [>!! <!!]]))
-
-(defn make-test-input
-  [])
+            [clojure.core.async :as a :refer [>!! <!!]]
+            [multiset.core :as ms]))
 
 (facts
   "about make-delay-node"
@@ -12,17 +10,44 @@
     "about make-delay-node with multisets"
     (let [source-node (core/make-source-node)
           delay-node (core/make-delay-node source-node)
-          test-channel (core/chan)]
-      (core/node-subscribe delay-node test-channel)
+          test-chan (core/chan)
+          source-chan (:in source-node)
+          id (:id source-node)]
 
-      (println (:in source-node))
+      (core/node-subscribe delay-node test-chan)
 
-      :truth => :truth
+      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset))})
+      (:multiset (:value (<!! test-chan))) => (ms/multiset)
 
-      ;(>!! test-channel {:destination})
+      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset 1))})
+      (:multiset (:value (<!! test-chan))) => (ms/multiset)
 
-      )
-    )
+      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset 1 2))})
+      (:multiset (:value (<!! test-chan))) => (ms/multiset 1)
+
+      (>!! source-chan {:destination id :value (core/make-multiset (ms/multiset 1 2 3))})
+      (:multiset (:value (<!! test-chan))) => (ms/multiset 1 2)))
   (facts
     "about make-delay-node with hashes"
-    ))
+    (let [source-node (core/make-source-node)
+          delay-node (core/make-delay-node source-node)
+          test-chan (core/chan)
+          source-chan (:in source-node)
+          id (:id source-node)]
+
+      (core/node-subscribe delay-node test-chan)
+
+      (>!! source-chan {:destination id :value (core/make-hash {})})
+      (:hash (:value (<!! test-chan))) => {}
+
+      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 1}})})
+      (:hash (:value (<!! test-chan))) => {}
+
+      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 1} 2 {:v 1}})})
+      (:hash (:value (<!! test-chan))) => {}
+
+      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 2} 2 {:v 1}})})
+      (:hash (:value (<!! test-chan))) => {1 {:v 1}}
+
+      (>!! source-chan {:destination id :value (core/make-hash {1 {:v 2} 2 {:v 2}})})
+      (:hash (:value (<!! test-chan))) => {1 {:v 1} 2 {:v 1}})))
