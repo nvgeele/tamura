3964057439ca78cab94be820650710530af8ea18
Nils Van Geele <nils@nvgeele.be>
2016-04-30 19:46:26 +0200

    More stuff for testing

--- test/tamura/core_test.clj
+++ test/tamura/core_test.clj
@@ -22,37 +22,44 @@
   [value]
   (>!! *source-chan* {:destination *source-id* :value value}))
 
+(defn receive-hash
+  []
+  (:hash (:value (<!! *test-chan*))))
+
+(defn receive-multiset
+  []
+  (:multiset (:value (<!! *test-chan*))))
+
 (facts
   "about make-delay-node"
   (facts
     "about make-delay-node with multisets"
     (test-node #(core/make-delay-node %)
       (send-to-source (core/make-multiset (ms/multiset)))
-      (:multiset (:value (<!! *test-chan*))) => (ms/multiset)
+      (receive-multiset) => (ms/multiset)
 
       (send-to-source (core/make-multiset (ms/multiset 1)))
-      (:multiset (:value (<!! *test-chan*))) => (ms/multiset)
+      (receive-multiset) => (ms/multiset)
 
       (send-to-source (core/make-multiset (ms/multiset 1 2)))
-      (:multiset (:value (<!! *test-chan*))) => (ms/multiset 1)
+      (receive-multiset) => (ms/multiset 1)
 
       (send-to-source (core/make-multiset (ms/multiset 1 2 3)))
-      (:multiset (:value (<!! *test-chan*))) => (ms/multiset 1 2)))
+      (receive-multiset) => (ms/multiset 1 2)))
 
   (facts
     "about make-delay-node with hashes"
     (test-node #(core/make-delay-node %)
       (send-to-source (core/make-hash {}))
-      (:hash (:value (<!! *test-chan*))) => {}
+      (receive-hash) => {}
 
       (send-to-source (core/make-hash {1 {:v 1}}))
-      (:hash (:value (<!! *test-chan*))) => {}
+      (receive-hash) => {}
 
       (send-to-source (core/make-hash {1 {:v 1} 2 {:v 1}}))
-      (:hash (:value (<!! *test-chan*))) => {}
+      (receive-hash) => {}
 
       (send-to-source (core/make-hash {1 {:v 2} 2 {:v 1}}))
-      (:hash (:value (<!! *test-chan*))) => {1 {:v 1}}
+      (receive-hash) => {1 {:v 1}}
 
       (send-to-source (core/make-hash {1 {:v 2} 2 {:v 2}}))
-      (:hash (:value (<!! *test-chan*))) => {1 {:v 1} 2 {:v 1}})))
