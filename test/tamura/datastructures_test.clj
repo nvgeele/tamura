21030772247c21364e8b438a7ab5c5e2e966003d
Nils Van Geele <nils@nvgeele.be>
2016-05-20 13:26:25 +0200

    More tests for datastructures

--- test/tamura/datastructures_test.clj
+++ test/tamura/datastructures_test.clj
@@ -31,20 +31,52 @@
       (do-op d/multiset-insert 2) => (ms/multiset 1 2)
       (do-op d/multiset-insert 3) => (ms/multiset 1 2 3)
       (do-op d/multiset-insert 4) => (ms/multiset 2 3 4)
-
       (do-op d/multiset-remove 4) => (ms/multiset 2 3)
       (do-op d/multiset-insert 2) => (ms/multiset 2 3 2)
-
       (do-op d/multiset-remove 2) => (ms/multiset 2 3)
       (do-op d/multiset-insert 1) => (ms/multiset 1 2 3)
       (do-op d/multiset-insert 1) => (ms/multiset 1 1 2)))
   (facts "about timed multisets"
-    )
+    (test-with (d/make-timed-multiset (t/seconds 2))
+      (do-op d/multiset-insert 1) => (ms/multiset 1)
+      (Thread/sleep 1000)
+      (do-op d/multiset-insert 2) => (ms/multiset 1 2)
+      (Thread/sleep 1100)
+      (do-op d/multiset-insert 3) => (ms/multiset 2 3)
+      (Thread/sleep 3000)
+      (do-op d/multiset-insert 4) => (ms/multiset 4)))
   (facts "about buffered & timed multisets"
-    ))
+    (test-with (d/make-timed-multiset (t/seconds 2) (d/make-buffered-multiset 2))
+      (do-op d/multiset-insert 1) => (ms/multiset 1)
+      (do-op d/multiset-insert 2) => (ms/multiset 1 2)
+      (do-op d/multiset-insert 3) => (ms/multiset 2 3)
+      (Thread/sleep 1000)
+      (do-op d/multiset-insert 4) => (ms/multiset 3 4)
+      (Thread/sleep 1100)
+      (do-op d/multiset-insert 5) => (ms/multiset 4 5)
+      (Thread/sleep 2100)
+      (do-op d/multiset-insert 1) => (ms/multiset 1))))
 
 (facts "about hashes"
-  (facts "about regular hashes")
-  (facts "about buffered hashes")
-  (facts "about timed hashes")
-  (facts "about buffered & timed hashes"))
+  (facts "about regular hashes"
+    (test-with (d/make-hash)
+      (do-op d/hash-insert :a 1) => {:a (ms/multiset 1)}
+      (do-op d/hash-insert :a 2) => {:a (ms/multiset 1 2)}
+      (d/to-multiset (d/hash-get @*coll* :a)) => (ms/multiset 1 2)
+      (do-op d/hash-insert :a 3)
+      (do-op d/hash-remove-element :a 3) => {:a (ms/multiset 1 2)}
+      (do-op d/hash-remove :a) => {}))
+  (facts "about buffered hashes"
+    (test-with (d/make-buffered-hash 2)
+      (do-op d/hash-insert :a 1) => {:a (ms/multiset 1)}
+      (do-op d/hash-insert :a 2) => {:a (ms/multiset 1 2)}
+      (do-op d/hash-insert :a 3) => {:a (ms/multiset 2 3)}
+      (do-op d/hash-insert :b 1) => {:a (ms/multiset 2 3) :b (ms/multiset 1)}
+      (do-op d/hash-insert :b 2) => {:a (ms/multiset 2 3) :b (ms/multiset 1 2)}
+      (do-op d/hash-insert :b 3) => {:a (ms/multiset 2 3) :b (ms/multiset 2 3)}))
+  (facts "about timed hashes"
+    (test-with (d/make-timed-hash (t/seconds 2))
+      ))
+  (facts "about buffered & timed hashes"
+    (test-with (d/make-timed-buffered-hash (t/seconds 2) 2)
+      )))
