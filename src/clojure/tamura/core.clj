8e9747fb98b597ac10da4a6dc51c3b4ba821ca81
Nils Van Geele <nils@nvgeele.be>
2016-06-04 19:47:02 +0200

    Improved the whole make-signal thing

--- src/clojure/tamura/core.clj
+++ src/clojure/tamura/core.clj
@@ -8,13 +8,13 @@
             [potemkin :as p]
             [tamura.macros :as macros]
             [tamura.node-types :as nt]
-            [tamura.values :as v]
     ;; We have to refer to runtimes as to make sure the constructors are registered and loaded
             [tamura.runtimes.clj :as crt])
   (:use [tamura.coordinator]
         [tamura.datastructures]
         [tamura.node]
-        [tamura.util]))
+        [tamura.util]
+        [tamura.values]))
 
 (p/import-vars
   [tamura.macros
@@ -56,10 +56,6 @@
   (core/reset! threads [])
   (>!! (:in *coordinator*) :reset))
 
-(core/defn- make-signal
-  [node]
-  (v/make-signal node))
-
 ;; TODO: redis sink
 ;; TODO: put coordinator in Node/Source record? *coordinator* is dynamic...
 ;; TODO: something something polling time
@@ -74,8 +70,8 @@
 (core/defn do-apply
   [f arg & args]
   (assert*
-    (andmap v/signal? (cons arg args)) "only signals from the second argument on")
-  (let [inputs (core/map v/value (cons arg args))
+    (andmap signal? (cons arg args)) "only signals from the second argument on")
+  (let [inputs (core/map signal-value (cons arg args))
         node (register-node! nt/do-apply nil [f] inputs)]
     (make-signal node)))
 
@@ -86,8 +82,8 @@
 (core/defn delay
   [arg]
   (assert*
-    (v/signal? arg) "argument to delay should be a signal")
-  (let [input (v/value arg)
+    (signal? arg) "argument to delay should be a signal")
+  (let [input (signal-value arg)
         return-type (:return-type (get-node input))
         node (register-node! nt/delay return-type [] [input])]
     (make-signal node)))
@@ -95,23 +91,23 @@
 (core/defn multiplicities
   [arg]
   (assert*
-    (v/signal? arg) "argument to multiplicities should be a signal"
-    (= (:return-type (get-node (v/value arg))) :multiset) "input for multiplicities must be multiset")
-  (make-signal (register-node! nt/multiplicities :multiset [] [(v/value arg)])))
+    (signal? arg) "argument to multiplicities should be a signal"
+    (= (:return-type (get-node (signal-value arg))) :multiset) "input for multiplicities must be multiset")
+  (make-signal (register-node! nt/multiplicities :multiset [] [(signal-value arg)])))
 
 ;; NOTE: Because multisets have no order, the function must be both commutative and associative
 ;; TODO: reduce code duplication
 (core/defn reduce
   ([source f]
    (assert*
-     (v/signal? source) "argument to reduce should be a signal"
-     (= (:return-type (get-node (v/value source))) :multiset) "input for reduce must be a multiset")
-   (make-signal (register-node! nt/reduce :multiset [f false] [(v/value source)])))
+     (signal? source) "argument to reduce should be a signal"
+     (= (:return-type (get-node (signal-value source))) :multiset) "input for reduce must be a multiset")
+   (make-signal (register-node! nt/reduce :multiset [f false] [(signal-value source)])))
   ([source f val]
    (assert*
-     (v/signal? source) "argument to reduce should be a signal"
-     (= (:return-type (get-node (v/value source))) :multiset) "input for reduce must be a multiset")
-   (make-signal (register-node! nt/reduce :multiset [f {:val val}] [(v/value source)]))))
+     (signal? source) "argument to reduce should be a signal"
+     (= (:return-type (get-node (signal-value source))) :multiset) "input for reduce must be a multiset")
+   (make-signal (register-node! nt/reduce :multiset [f {:val val}] [(signal-value source)]))))
 
 ;; We *must* work with a node that signals the coordinator to ensure correct propagation in situations where
 ;; nodes depend on a throttle signal and one or more other signals.
@@ -121,10 +117,10 @@
 (core/defn throttle
   [signal ms]
   (assert*
-    (v/signal? signal) "first argument of throttle must be signal")
+    (signal? signal) "first argument of throttle must be signal")
   (let [trigger (register-source! nt/source :multiset [:multiset])
-        return-type (:return-type (get-node (v/value signal)))
-        node (register-node! nt/throttle return-type [ms] [(v/value signal) trigger])]
+        return-type (:return-type (get-node (signal-value signal)))
+        node (register-node! nt/throttle return-type [ms] [(signal-value signal) trigger])]
     (threadloop []
       (Thread/sleep ms)
       (>!! (:in *coordinator*) {:destination trigger :value nil})
@@ -136,96 +132,96 @@
 (core/defn buffer
   [sig size]
   (assert*
-    (v/signal? sig) "first argument to buffer should be a signal"
-    (nt/source? (:node-type (get-node (v/value sig)))) "input for buffer node should be a source")
-  (make-signal (register-node! nt/buffer (:return-type (get-node (v/value sig))) [size] [(v/value sig)])))
+    (signal? sig) "first argument to buffer should be a signal"
+    (nt/source? (:node-type (get-node (signal-value sig)))) "input for buffer node should be a source")
+  (make-signal (register-node! nt/buffer (:return-type (get-node (signal-value sig))) [size] [(signal-value sig)])))
 
 (core/defn diff-add
   [sig]
-  (let [input-node-type (:node-type (get-node (v/value sig)))]
+  (let [input-node-type (:node-type (get-node (signal-value sig)))]
     (assert*
-      (v/signal? sig) "first argument to diff-add should be a signal"
+      (signal? sig) "first argument to diff-add should be a signal"
       (or (nt/source? input-node-type)
           (nt/buffer? input-node-type)
           (nt/delay? input-node-type))
       "input for diff-add node should be a source, buffer, or delay")
-    (make-signal (register-node! nt/diff-add (:return-type (get-node (v/value sig))) [] [(v/value sig)]))))
+    (make-signal (register-node! nt/diff-add (:return-type (get-node (signal-value sig))) [] [(signal-value sig)]))))
 
 (core/defn diff-remove
   [sig]
-  (let [input-node-type (:node-type (get-node (v/value sig)))]
+  (let [input-node-type (:node-type (get-node (signal-value sig)))]
     (assert*
-      (v/signal? sig) "first argument to diff-remove should be a signal"
+      (signal? sig) "first argument to diff-remove should be a signal"
       (or (nt/source? input-node-type)
           (nt/buffer? input-node-type)
           (nt/delay? input-node-type))
       "input for diff-remove node should be a source, buffer, or delay")
-    (make-signal (register-node! nt/diff-remove (:return-type (get-node (v/value sig))) [] [(v/value sig)]))))
+    (make-signal (register-node! nt/diff-remove (:return-type (get-node (signal-value sig))) [] [(signal-value sig)]))))
 
 ;; TODO: make sure size > buffer size of buffer or source?
 (core/defn filter-key-size
   [source size]
   (assert*
-    (v/signal? source) "argument to filter-key-size should be a signal"
-    (= (:return-type (get-node (v/value source))) :hash) "input for filter-key-size must be a hash")
-  (make-signal (register-node! nt/filter-key-size :hash [size] [(v/value source)])))
+    (signal? source) "argument to filter-key-size should be a signal"
+    (= (:return-type (get-node (signal-value source))) :hash) "input for filter-key-size must be a hash")
+  (make-signal (register-node! nt/filter-key-size :hash [size] [(signal-value source)])))
 
 ;; NOTE: Because multisets have no order, the function must be both commutative and associative
 ;; TODO: reduce code duplication
 (core/defn reduce-by-key
   ([source f]
    (assert*
-     (v/signal? source) "argument to reduce-by-key should be a signal"
-     (= (:return-type (get-node (v/value source))) :hash) "input for reduce-by-key must be a hash")
-   (make-signal (register-node! nt/reduce-by-key :hash [f false] [(v/value source)])))
+     (signal? source) "argument to reduce-by-key should be a signal"
+     (= (:return-type (get-node (signal-value source))) :hash) "input for reduce-by-key must be a hash")
+   (make-signal (register-node! nt/reduce-by-key :hash [f false] [(signal-value source)])))
   ([source f val]
    (assert*
-     (v/signal? source) "argument to reduce-by-key should be a signal"
-     (= (:return-type (get-node (v/value source))) :hash) "input for reduce-by-key must be a hash")
-   (make-signal (register-node! nt/reduce-by-key :hash [f {:val val}] [(v/value source)]))))
+     (signal? source) "argument to reduce-by-key should be a signal"
+     (= (:return-type (get-node (signal-value source))) :hash) "input for reduce-by-key must be a hash")
+   (make-signal (register-node! nt/reduce-by-key :hash [f {:val val}] [(signal-value source)]))))
 
 (core/defn hash-to-multiset
   [source]
   (assert*
-    (v/signal? source) "argument to hash-to-multiset should be a signal"
-    (= (:return-type (get-node (v/value source))) :hash) "input for hash-to-multiset must be a hash")
-  (make-signal (register-node! nt/hash-to-multiset :multiset [] [(v/value source)])))
+    (signal? source) "argument to hash-to-multiset should be a signal"
+    (= (:return-type (get-node (signal-value source))) :hash) "input for hash-to-multiset must be a hash")
+  (make-signal (register-node! nt/hash-to-multiset :multiset [] [(signal-value source)])))
 
 ;; TODO: like reduce, make it work for regular collections too
 (core/defn map
   [source f]
   (assert*
-    (v/signal? source) "argument to map should be a signal"
-    (= (:return-type (get-node (v/value source))) :multiset) "input for map must be a multiset")
-  (make-signal (register-node! nt/map :multiset [f] [(v/value source)])))
+    (signal? source) "argument to map should be a signal"
+    (= (:return-type (get-node (signal-value source))) :multiset) "input for map must be a multiset")
+  (make-signal (register-node! nt/map :multiset [f] [(signal-value source)])))
 
 (core/defn map-by-key
   [source f]
   (assert*
-    (v/signal? source) "argument to map-by-key should be a signal"
-    (= (:return-type (get-node (v/value source))) :multiset) "input for map-by-key must be a hash")
-  (make-signal (register-node! nt/map-by-key :hash [f] [(v/value source)])))
+    (signal? source) "argument to map-by-key should be a signal"
+    (= (:return-type (get-node (signal-value source))) :multiset) "input for map-by-key must be a hash")
+  (make-signal (register-node! nt/map-by-key :hash [f] [(signal-value source)])))
 
 ;; TODO: like reduce, make it work for regular collections too
 (core/defn filter
   [source f]
   (assert*
-    (v/signal? source) "argument to filter should be a signal"
-    (= (:return-type (get-node (v/value source))) :multiset) "input for filter must be a multiset")
-  (make-signal (register-node! nt/filter :multiset [f] [(v/value source)])))
+    (signal? source) "argument to filter should be a signal"
+    (= (:return-type (get-node (signal-value source))) :multiset) "input for filter must be a multiset")
+  (make-signal (register-node! nt/filter :multiset [f] [(signal-value source)])))
 
 (core/defn filter-by-key
   [source f]
   (assert*
-    (v/signal? source) "argument to filter-by-key should be a signal"
-    (= (:return-type (get-node (v/value source))) :hash) "input for filter-by-key must be a hash")
-  (make-signal (register-node! nt/filter-by-key :hash [f] [(v/value source)])))
+    (signal? source) "argument to filter-by-key should be a signal"
+    (= (:return-type (get-node (signal-value source))) :hash) "input for filter-by-key must be a hash")
+  (make-signal (register-node! nt/filter-by-key :hash [f] [(signal-value source)])))
 
 (defn- print*
   [signal form]
   (assert*
-    (v/signal? signal) "argument to print must be a signal")
-  (make-signal (register-sink! nt/print [form] [(v/value signal)])))
+    (signal? signal) "argument to print must be a signal")
+  (make-signal (register-sink! nt/print [form] [(signal-value signal)])))
 
 (defmacro print
   [input-form]
--- src/clojure/tamura/values.clj
+++ src/clojure/tamura/values.clj
@@ -1,40 +1,13 @@
-(ns tamura.values
-  (:require [clojure.string :as str]))
+(ns tamura.values)
 
-;; TODO: type of data produced in signal stuffs
+(defrecord Signal [value])
 
-(defprotocol TaggedValue
-  (tag [this])
-  (value [this]))
-
-(deftype TValue [tag value]
-  TaggedValue
-  (tag [this] tag)
-  (value [this] value))
-
-(defn make-tagged
-  [tag value]
-  (TValue. tag value))
-
-(defmacro deftag
-  [name & ancestors]
-  (let [tkwd (keyword (str *ns*) (str name))
-        psym (symbol (str *ns*) (str (str/lower-case (str name)) "?"))
-        fsym (symbol (str *ns*) (str "make-" (str/lower-case (str name))))]
-    `(do
-       (def ~name ~tkwd)
-       (defn ~fsym
-         [v#]
-         (make-tagged ~tkwd v#))
-       (defn ~psym
-         [v#]
-         (and (= (type v#) TValue)
-              (isa? (tag v#) ~tkwd)))
-       (doseq [ancestor# [~@ancestors]]
-         (derive ~name ancestor#)))))
-
-(deftag Value)
-(deftag Constant Value)
-(deftag Signal Value)
-(deftag EventStream Signal)
-(deftag Behaviour Signal)
+(defn signal?
+  [x]
+  (instance? Signal x))
+(defn make-signal
+  [node]
+  (Signal. node))
+(defn signal-value
+  [signal]
+  (:value signal))
