75e5698ac1c4c69a71c7dcc23b0797d18d8a19fd
Nils Van Geele <nils@nvgeele.be>
2016-06-04 18:57:17 +0200

    Fixed indentation for coordinator

--- src/clojure/tamura/coordinator.clj
+++ src/clojure/tamura/coordinator.clj
@@ -12,30 +12,30 @@
     (go-loop [msg (<! in)
               started? false
               sources []]
-             (log/debug (str "coordinator received: " msg))
-             (match msg
-                    {:new-source source-chan}
-                    (if started?
-                      (throw (Exception. "can not add new sources when already running"))
-                      (recur (<! in) started? (cons source-chan sources)))
+      (log/debug (str "coordinator received: " msg))
+      (match msg
+        {:new-source source-chan}
+        (if started?
+          (throw (Exception. "can not add new sources when already running"))
+          (recur (<! in) started? (cons source-chan sources)))
 
-                    {:destination id :value value}
-                    (do (when started?
-                          (doseq [source sources]
-                            (>! source msg)))
-                        (recur (<! in) started? sources))
+        {:destination id :value value}
+        (do (when started?
+              (doseq [source sources]
+                (>! source msg)))
+            (recur (<! in) started? sources))
 
-                    {:started? reply-channel}
-                    (do (>! reply-channel started?)
-                        (recur (<! in) started? sources))
+        {:started? reply-channel}
+        (do (>! reply-channel started?)
+            (recur (<! in) started? sources))
 
-                    :start (recur (<! in) true sources)
+        :start (recur (<! in) true sources)
 
-                    :stop (recur (<! in) false [])
+        :stop (recur (<! in) false [])
 
-                    :reset (recur (<! in) false [])
+        :reset (recur (<! in) false [])
 
-                    :else (recur (<! in) started? sources)))
+        :else (recur (<! in) started? sources)))
     (Coordinator. in)))
 
 (def ^:dynamic *coordinator* (make-coordinator))
